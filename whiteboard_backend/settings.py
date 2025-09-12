17e129e0204855f9e772cdaf5a34e452ac191dd9
deusxkvg <vickkaniu@gmail.com>
2025-09-12 19:45:45 +0300

    piick which database to use

--- whiteboard_backend/settings.py
+++ whiteboard_backend/settings.py
@@ -88,17 +88,21 @@ import dj_database_url
 
 AUTH_USER_MODEL = "users.CustomUser"
 
-"""
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ["DATABASE_URL"],
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
-"""
-
-DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
-
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
